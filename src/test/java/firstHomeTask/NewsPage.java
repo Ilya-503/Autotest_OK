a305c83432e44c90f209b94a6b53a57406bfa72a
Ilya Shumilov <shumilov.iv@mail.ru>
2022-10-23 01:11:00 +0300

    changed newsPage selector

--- src/test/java/firstHomeTask/NewsPage.java
+++ src/test/java/firstHomeTask/NewsPage.java
@@ -2,12 +2,12 @@ package firstHomeTask;
 
 import com.codeborne.selenide.SelenideElement;
 
-import static com.codeborne.selenide.Selectors.byAttribute;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 
 public class NewsPage {
 
-    private SelenideElement noteField = $(byAttribute("data-l", "t,mainpf_open"));
+    private SelenideElement noteField = $(byText("Напишите заметку"));
 
     public SelenideElement getNoteFiled() {
         return noteField;
