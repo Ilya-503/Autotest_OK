f8d9d71dec7cb8a04660d18bbe5eff1a9617ba53
Ilya Shumilov <shumilov.iv@mail.ru>
2022-11-15 20:09:45 +0300

    fixed musicUpperToolBar

--- src/test/java/pages/musicPage/UpperMusicToolBar.java
+++ src/test/java/pages/musicPage/UpperMusicToolBar.java
@@ -1,13 +1,7 @@
 package pages.musicPage;
 
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.WebDriverRunner;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.Keys;
-
-import java.awt.*;
-import java.security.Key;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.$;
@@ -16,44 +10,44 @@ public class UpperMusicToolBar {
 
     private static SelenideElement rootElem;
 
-    private static final SelenideElement INPUT_FIELD =
-            $(byAttribute("data-tsid","inner_input"));
+    private static final By INPUT_FIELD =
+            byAttribute("data-tsid","inner_input");
 
-    private static final SelenideElement CURRENT_PLAYLIST_BTN =
-            $(byAttribute("data-tsid", "current_playlist"));
+    private static final By CURRENT_PLAYLIST_BTN =
+            byAttribute("data-tsid", "current_playlist");
 
-    private static final SelenideElement SHARE_TRAC_BTN =
-            $(byAttribute("data-tsid", "share_track"));
+    private static final By SHARE_TRAC_BTN =
+            byAttribute("data-tsid", "share_track");
 
-    private static final SelenideElement ADD_TRACK_BTN =
-            $(byAttribute("data-tsid", "add_track"));
+    private static final By ADD_TRACK_BTN =
+            byAttribute("data-tsid", "add_track");
 
-    private static final SelenideElement TRACK_NAME =
-            $(byAttribute("data-tsid", "track_name"));
+    private static final By TRACK_NAME =
+            byAttribute("data-tsid", "track_name");
 
-    private static final SelenideElement TRACK_AUTHOR =
-            $(byClassName("artist"));
+    private static final By TRACK_AUTHOR =
+            byClassName("artist");
 
-    private static final SelenideElement NEXT_TRACK_BTN =
-            $(byAttribute("data-tsid", "forward_button"));
+    private static final By NEXT_TRACK_BTN =
+            byAttribute("data-tsid", "forward_button");
 
-    private static final SelenideElement PREV_TRACK_BTN =
-            $(byAttribute("data-tsid", "backward_button"));
+    private static final By PREV_TRACK_BTN =
+            byAttribute("data-tsid", "backward_button");
 
     public UpperMusicToolBar(SelenideElement rootElem) {
         this.rootElem = rootElem;
     }
 
     public void findTrack(String trackName) {
-        INPUT_FIELD.setValue(trackName);
+        $(INPUT_FIELD).setValue(trackName);
         try {
             Thread.sleep(500);     // 'search' icon changes too fast :(
         } catch (Exception e) {}
-        INPUT_FIELD.pressEnter();
+        $(INPUT_FIELD).pressEnter();
     }
 
     public void clearSearchField() {
-        INPUT_FIELD.clear();
+        $(INPUT_FIELD).clear();
     }
 }
 
