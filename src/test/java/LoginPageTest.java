3f94a638dcb3651db6b3402efafb98addde8cd3d
Ilya Shumilov <shumilov.iv@mail.ru>
2022-12-04 20:20:58 +0300

    fix some bug

--- src/test/java/LoginPageTest.java
+++ src/test/java/LoginPageTest.java
@@ -1,9 +1,7 @@
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import pages.loginPage.LoginPage;
 import pages.mainPage.MainPage;
 import org.junit.jupiter.api.BeforeEach;
